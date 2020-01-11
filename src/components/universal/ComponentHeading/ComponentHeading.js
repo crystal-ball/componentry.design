65838af90073901388d96cb813b8ed9ffb1b4ece
Dan Hedgecock <hedgecock.d@gmail.com>
2020-01-10 19:00:56 -0800

    New: Add a screen for each component 📝

--- src/components/universal/ComponentHeading/ComponentHeading.js
+++ src/components/universal/ComponentHeading/ComponentHeading.js
@@ -7,13 +7,14 @@ import { css } from '@emotion/core'
 const headingStyles = ({ typographyColors, typography }) => css`
   font-family: ${typography.fontFamilyDisplay};
   color: ${typographyColors.radvender};
-  letter-spacing: 8px;
-  text-align: left;
-  margin-top: 80px;
 `
 
 export default function ComponentHeading({ name }) {
-  return <Heading className='raddow-1' css={headingStyles}>{`<${name}/>`}</Heading>
+  return (
+    <Heading className='raddow-1' my='xl' textAlign='right' css={headingStyles}>
+      {`<${name}/>`}
+    </Heading>
+  )
 }
 
 ComponentHeading.propTypes = {
