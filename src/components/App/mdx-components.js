11506ec0d970b74b6e0db1d2fed0809d7d85dbf6
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-01-14 08:25:08 -0800

    New: Component heading includes components list (#56)

--- src/components/App/mdx-components.js
+++ src/components/App/mdx-components.js
@@ -1,15 +1,27 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react'
 import * as componentry from 'componentry' // eslint-disable-line
 
 import { CodeBlock, ComponentHeading, Link } from '@/components/universal'
 
-const { Theme, Media, setupOutlineHandlers, ...componentryComponents } = componentry
+const {
+  Heading,
+  Theme,
+  Media,
+  setupOutlineHandlers,
+  ...componentryComponents
+} = componentry
 
 // Include all component library components in MDX context for slimmer files
 export const components = {
   ...componentryComponents,
+  Heading,
   Link,
   ComponentHeading,
   code: CodeBlock,
   pre: props => <div {...props} />,
+  h1: props => <Heading variant='heading-1' {...props} />,
+  h2: props => <Heading variant='heading-2' {...props} />,
+  h3: props => <Heading variant='heading-3' {...props} />,
+  h4: props => <Heading variant='heading-4' {...props} />,
 }
