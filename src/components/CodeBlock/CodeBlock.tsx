60b0be5778603b9ec24e5c0edb3a3b9073fc4979
Dan Hedgecock <Hedgecock.D@gmail.com>
2022-01-23 12:27:09 -0800

    New: Syntax highlighting support (#175)

---
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,27 @@
+import { toH } from 'hast-to-hyperscript'
+import { createElement } from 'react'
+import { refractor } from 'refractor/lib/core'
+import tsx from 'refractor/lang/tsx'
+import css from 'refractor/lang/css'
+
+refractor.register(css)
+refractor.register(tsx)
+
+export const CodeBlock = ({ code, language }: CodeBlockProps) => {
+  let contents
+  if (language) {
+    contents = toH(createElement, refractor.highlight(code, language))
+  } else {
+    contents = code
+  }
+
+  return (
+    <pre className={`language-${language ?? 'none'} rounded`}>
+      <code>{contents}</code>
+    </pre>
+  )
+}
+type CodeBlockProps = {
+  code: string
+  language?: 'css' | 'shell' | 'tsx'
+}
--- src/pages/getting-started/installation.tsx
+++ src/pages/getting-started/installation.tsx
@@ -4,6 +4,7 @@ import { Flex, Text } from 'componentry'
 import { AppHead } from '@/components/AppHead/AppHead'
 import Header from '@/components/Header/Header'
 import { ScreenContainer } from '@/components/ScreenContainer/ScreenContainer'
+import { CodeBlock } from '@/components/CodeBlock/CodeBlock'
 
 export default function Installation(): JSX.Element {
   return (
@@ -18,6 +19,11 @@ export default function Installation(): JSX.Element {
         <Text variant='lead' mt={3}>
           How to install and start using Componentry in your project.
         </Text>
+        <Text variant='h3'>Install from npm</Text>
+        <CodeBlock code='npm install componentry' />
+
+        <Text variant='h3'>Include styles</Text>
+        <CodeBlock code='@componentry components;' language='css' />
       </Flex>
     </ScreenContainer>
   )
