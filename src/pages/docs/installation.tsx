f10058687364acd5c75a7f5941712e2ad757db99
Dan Hedgecock <hedgecock.d@gmail.com>
2022-02-09 15:18:39 -0800

    Docs: Installation and theme customization

--- src/pages/docs/installation.tsx
+++ src/pages/docs/installation.tsx
@@ -1,13 +1,16 @@
-import { Text } from 'componentry'
+import { Grid, Text } from 'componentry'
 
 import { DocsScreenLayout } from '@/components/Layout/DocsScreen'
 import { CodeBlock } from '@/components/CodeBlock/CodeBlock'
+import { ContentSection } from '@/components/Layout/ContentSection'
 
 const postcssConfig = `{
   "plugins": ["componentry/postcss", "autoprefixer"]
 }`
 
 const directives = `/* The "components" param will include all component styles */
+@componentry foundation;
+
 @componentry components;
 
 /* Or each component style can be selectively included */
@@ -35,20 +38,57 @@ export default function Installation() {
       <Text variant='h1' mt={1}>
         Installation
       </Text>
-      <Text variant='lead' mt={3}>
-        Install componentry and import the styles with PostCSS
-      </Text>
-      <Text variant='h3'>Install Componentry and PostCSS</Text>
-      <CodeBlock code='npm install componentry postcss autoprefixer' />
 
-      <Text variant='h3'>Add Componentry to your PostCSS config</Text>
-      <CodeBlock code={postcssConfig} language='tsx' />
+      <ContentSection>
+        <Text variant='lead' mt={3}>
+          Install componentry and import the styles. Componentry uses PostCSS to convert
+          custom <code>@componentry</code> directives to CSS.
+        </Text>
+      </ContentSection>
 
-      <Text variant='h3'>Add the Componentry component directives to your CSS</Text>
-      <CodeBlock code={directives} language='css' />
+      {/* --- NPM --- */}
+      <Grid width='full' gap={3} mt={6} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>1. Install Componentry and PostCSS</Text>
+        </div>
+        <CodeBlock code='npm install componentry postcss autoprefixer' />
+      </Grid>
 
-      <Text variant='h3'>Build something radical</Text>
-      <CodeBlock code={gettingStarted} language='tsx' />
+      {/* --- FOUNDATION --- */}
+      <Grid width='full' gap={3} mt={3} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>2. Add Componentry to your PostCSS config</Text>
+          <Text mt={1} ml={1}>
+            Include Componentry's PostCSS plugin in your config. The plugin converts the
+            <code>@componentry</code> directives into styles.
+          </Text>
+        </div>
+        <CodeBlock code={postcssConfig} language='tsx' />
+      </Grid>
+
+      {/* --- DIRECTIVES --- */}
+      <Grid width='full' gap={3} mt={3} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>3. Add directives to your CSS</Text>
+          <Text mt={1} ml={1}>
+            The <code>foundation</code> directive is converted to the base styles, and the{' '}
+            <code>components</code>
+            directive is converted to the component styles.
+          </Text>
+        </div>
+        <CodeBlock code={directives} language='css' />
+      </Grid>
+
+      {/* --- DIRECTIVES --- */}
+      <Grid width='full' gap={3} mt={3} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>4. Build something radical</Text>
+          <Text mt={1} ml={1}>
+            You're ready to start using Componentry.
+          </Text>
+        </div>
+        <CodeBlock code={gettingStarted} language='tsx' />
+      </Grid>
     </DocsScreenLayout>
   )
 }
--- src/pages/docs/theme-customization.tsx
+++ src/pages/docs/theme-customization.tsx
@@ -1,6 +1,133 @@
-import { Text } from 'componentry'
+import { Grid, Text } from 'componentry'
 
 import { DocsScreenLayout } from '@/components/Layout/DocsScreen'
+import { ContentSection } from '@/components/Layout/ContentSection'
+import { CodeBlock } from '@/components/CodeBlock/CodeBlock'
+
+const config = `// componentry.config.js
+module.exports = {
+  theme: {},
+}`
+
+const override = `// componentry.config.js
+module.exports = {
+  theme: {
+    colors: {
+      link: '#7d77ff'
+    }
+  },
+}
+`
+
+const extend = `// componentry.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        link: '#7d77ff'
+      }
+    }
+  },
+}`
+
+const theme = `const theme = {
+  zIndex: {
+    modal: 10,
+  },
+  // --- COLORS
+  colors: {
+    current: 'currentColor',
+    transparent: 'transparent',
+    background: '#fff',
+    inverse: '#eff',
+    gray: {
+      100: '#eff2f3',
+      200: '#d7dfe2',
+      // ...
+    },
+    primary: {
+      100: '#f2f1ff',
+      200: '#dfddff',
+      // ...
+    },
+    info: {
+      100: '#dbeafe',
+      200: '#bfdbfe',
+      // ...
+    },
+    success: {
+      100: '#f4f9ed',
+      200: '#e4f1d2',
+      // ...
+    },
+    warning: {
+      100: '#fdf8ec',
+      200: '#fbedcf',
+      // ...
+    },
+    error: {
+      100: '#fbeeed',
+      200: '#f6d4d3',
+      // ...
+    },
+  },
+
+  // --- SPACING
+  spacing: {
+    0: 0,
+    0.5: '0.25rem', // 4px
+    1: '0.5rem', // 8px
+    1.5: '0.75rem', // 12px
+    2: '1rem', // 16px
+    3: '1.5rem', // 24px
+    4: '2rem', // 32px
+    5: '2.5rem', // 40px
+    6: '3rem', // 48px
+  },
+
+  // --- TYPOGRAPHY
+  fontFamily: {
+    body: "system-ui, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'",
+    monospace: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+  },
+  fontSize: {
+    base: '1rem', // HTML base size
+    small: '0.875rem',
+    large: '1.25rem',
+    h1: '3rem',
+    h2: '2rem',
+    h3: '1.5rem',
+    body: '1rem',
+  },
+  fontWeight: {
+    light: 300,
+    normal: 400,
+    bold: 700,
+  },
+  lineHeight: {
+    none: 1,
+    normal: 1.5,
+  },
+
+  // --- BORDERS
+  borderWidth: {
+    DEFAULT: '1px',
+  },
+  borderColor: {
+    DEFAULT: '#607d8b',
+  },
+  borderRadius: {
+    DEFAULT: '4px',
+    sm: '2px',
+    md: '4px',
+    lg: '6px',
+  },
+
+  // --- SHADOWS
+  boxShadow: {
+    DEFAULT: '0 0.5rem 1rem rgba(73, 80, 87, 0.15)',
+  },
+}`
 
 export default function ThemeCustomization() {
   return (
@@ -9,14 +136,56 @@ export default function ThemeCustomization() {
       <Text variant='h1' mt={1}>
         Theme customization
       </Text>
-      <Text variant='lead' mt={3}>
-        Componentry uses a base theme for generating styles that can be customized to
-        match your theme. You can then use that theme throughout your application using
-        the <code>useTheme</code> hook and Tailwind's <code>theme()</code> directive.
-      </Text>
-      <Text variant='h2'>Theme Configuration</Text>
-      1. Theme is configured in <code>componentry.config.js</code>
-      2. Default theme shape
+      <ContentSection>
+        <Text variant='lead' mt={3}>
+          Componentry uses a base theme for generating styles that can be customized to
+          match your theme. You can then use that theme throughout your application using
+          the <code>useTheme</code> hook and Tailwind's <code>theme()</code> directive.
+        </Text>
+      </ContentSection>
+      {/* --- FILE --- */}
+      <Grid width='full' gap={3} mt={6} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>
+            1. Create a <code>componentry.config.js</code>
+          </Text>
+          <Text mt={1} ml={1}>
+            Componentry will merge the values from your config with the library defaults.
+          </Text>
+        </div>
+        <CodeBlock code={config} language='tsx' />
+      </Grid>
+      {/* --- OVERRIDE --- */}
+      <Grid width='full' gap={3} mt={6} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>2. Override theme values</Text>
+          <Text mt={1} ml={1}>
+            Setting a theme value will override all the default values, this allows
+            constraining the theme values to only the ones you define.
+          </Text>
+        </div>
+        <CodeBlock code={override} language='tsx' />
+      </Grid>
+      {/* --- EXTENDS --- */}
+      <Grid width='full' gap={3} mt={6} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>2. Extend theme values</Text>
+          <Text mt={1} ml={1}>
+            Default theme values can be extended using the <code>extend</code> field, this
+            allows you to add custom theme values to the Componentry default set.
+          </Text>
+        </div>
+        <CodeBlock code={extend} language='tsx' />
+      </Grid>
+      {/* --- DEFAULTS --- */}
+      <ContentSection>
+        <Text variant='h2'>Theme defaults</Text>
+        <Text mb={2}>
+          Componentry's default theme shape includes fields for setting z-index, color,
+          spacing, and typography values.
+        </Text>
+        <CodeBlock code={theme} language='tsx' />
+      </ContentSection>
     </DocsScreenLayout>
   )
 }
