35dcface967e6839608d8d4b81c24aaef21636eb
Dan Hedgecock <Hedgecock.D@gmail.com>
2022-03-04 06:01:10 -0800

    V4 blog post (#194)

--- src/components/Layout/BlogScreen.tsx
+++ src/components/Layout/BlogScreen.tsx
@@ -6,7 +6,7 @@ export function BlogScreenLayout({ children }: ScreenContainerProps) {
   return (
     <Flex direction='column' className='min-h-screen'>
       <Header />
-      <Flex px={16} mt={3}>
+      <Flex px={16} mt={3} mx='auto'>
         <Flex pb={6} direction='column'>
           {children}
         </Flex>
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,29 @@
+import { Flex, Text } from 'componentry'
+import NextLink from 'next/link'
 import { BlogScreenLayout } from '@/components/Layout/BlogScreen'
 
 export default function Blog() {
   return (
     <BlogScreenLayout>
-      <div>Long-form writing...</div>
+      <Flex direction='column' gap={4} align='center'>
+        <Text variant='h2' textAlign='center' my={6}>
+          Componentry release updates
+        </Text>
+        <NextLink href='/blog/v4'>
+          <Flex direction='column' style={{ maxWidth: '600px', cursor: 'pointer' }}>
+            <Flex mt={0.5} justify='between' align='center'>
+              <Text variant='h3' color='link'>
+                Componentry v4.0.0
+              </Text>
+              <Text variant='overline'>March 4, 2022</Text>
+            </Flex>
+            <Text mt={1.5}>
+              Componentry V4 enables a significantly upgraded workflow for customizing
+              styles by using PostCSS instead of SASS for styles processing.
+            </Text>
+          </Flex>
+        </NextLink>
+      </Flex>
     </BlogScreenLayout>
   )
 }
