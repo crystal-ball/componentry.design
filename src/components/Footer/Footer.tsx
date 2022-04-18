275fe0f3dbca76a15764708acbcf01df4b0f32d0
Dan Hedgecock <Hedgecock.D@gmail.com>
2022-04-18 16:54:11 -0700

    Link to Vercel in banner (#208)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import { Flex, Text } from 'componentry'
 export function Footer() {
   return (
     <Flex as='footer' justify='center' p={3}>
-      <Text>▲ Powered by Vercel | Made with magic</Text>
+      <Text>
+        <a href='https://vercel.com/?utm_source=crystal-ball&utm_campaign=oss'>
+          ▲ Powered by Vercel
+        </a>{' '}
+        | Made with magic
+      </Text>
     </Flex>
   )
 }
