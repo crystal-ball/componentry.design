e06c7d3873da70dceea1642b8a6b1b3ce1ee3c81
Dan Hedgecock <hedgecock.d@gmail.com>
2022-04-27 05:32:47 -0700

    Document simpler setup workflow

--- src/pages/docs/theme-customization.tsx
+++ src/pages/docs/theme-customization.tsx
@@ -1,4 +1,4 @@
-import { Grid, Text } from 'componentry'
+import { Grid, Link, Text } from 'componentry'
 
 import { CodeBlock } from '@/components/CodeBlock/CodeBlock'
 import { ContentSection } from '@/components/Layout/ContentSection'
@@ -30,8 +30,21 @@ module.exports = {
   },
 }`
 
+const augment = `// componentry-types.ts
+import theme from './path/to/theme'
+
+// Use module augmentation along with provided ThemeOverrides to update the Theme type
+declare module 'componentry/types/theme/theme' {
+  type CustomTheme = typeof theme
+  export interface ThemeOverrides extends CustomTheme {}
+}`
+
 const theme = `const theme = {
+  screens: {
+    lg: '1200px',
+  },
   zIndex: {
+    auto: 'auto',
     modal: 10,
   },
   // --- COLORS
@@ -72,17 +85,60 @@ const theme = `const theme = {
     },
   },
 
+  backgroundColor: undefined
+  borderColor: undefined
+  textColor: undefined,
+
+  // --- FLEX/GRID
+  flexGrow: { DEFAULT: 1, 0: 0 },
+  flexShrink: { DEFAULT: 1, 0: 0 },
+
   // --- SPACING
   spacing: {
+    auto: 'auto',
+    px: '1px',
     0: 0,
-    0.5: '0.25rem', // 4px
-    1: '0.5rem', // 8px
-    1.5: '0.75rem', // 12px
-    2: '1rem', // 16px
-    3: '1.5rem', // 24px
-    4: '2rem', // 32px
-    5: '2.5rem', // 40px
-    6: '3rem', // 48px
+    0.5: '0.125rem', // 2px
+    1: '0.25rem', // 4px
+    1.5: '0.375rem', // 6px
+    2: '0.5rem', // 8px
+    2.5: '0.625rem', // 10px
+    3: '0.75rem', // 12px
+    4: '1rem', // 16px
+    5: '1.25rem', // 20px
+    6: '1.5rem', // 24px
+    8: '2rem', // 32px
+    10: '2.5rem', // 40px
+    12: '3rem', // 48px
+    14: '3.5rem', // 56px
+    16: '4rem', // 64px
+    20: '5rem', // 80px
+    24: '6rem', // 96px
+    32: '8rem', // 128px
+    48: '12rem', // 192px
+    64: '16rem', // 256px
+  },
+  spacingRatio: undefined,
+
+  // --- SIZING
+  height: {
+    0: 0,
+    auto: 'auto',
+    full: '100%',
+    screen: '100vh',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  },
+  width: {
+    0: 0,
+    auto: 'auto',
+    full: '100%',
+    screen: '100vw',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+    prose: '65ch',
   },
 
   // --- TYPOGRAPHY
@@ -104,23 +160,48 @@ const theme = `const theme = {
     normal: 400,
     bold: 700,
   },
+  letterSpacing: {
+    tighter: '-0.05em',
+    tight: '-0.025em',
+    normal: '0em',
+    wide: '0.025em',
+    wider: '0.05em',
+    widest: '0.1em',
+  },
   lineHeight: {
     none: 1,
     normal: 1.5,
   },
 
   // --- BORDERS
-  borderWidth: {
-    DEFAULT: '1px',
-  },
-  borderColor: {
-    DEFAULT: '#607d8b',
+  border: {
+    DEFAULT: '1px solid #607d8b',
   },
   borderRadius: {
-    DEFAULT: '4px',
-    sm: '2px',
-    md: '4px',
-    lg: '6px',
+    DEFAULT: '0.25rem', // 4px
+    none: '0px',
+    full: '9999px',
+    sm: '0.125rem', // 2px
+    md: '0.375rem', // 6px
+    lg: '6px', // 8px
+    xl: '0.75rem', // 12px
+    '2xl': '1rem', // 16px
+    '3xl': '1.5rem', // 24px
+  },
+  borderStyle: {
+    dashed: 'dashed',
+    dotted: 'dotted',
+    double: 'double',
+    hidden: 'hidden',
+    none: 'none',
+    solid: 'solid',
+  },
+  borderWidth: {
+    0: 0,
+    1: '1px',
+    2: '2px',
+    4: '4px',
+    8: '8px',
   },
 
   // --- SHADOWS
@@ -169,13 +250,28 @@ export default function ThemeCustomization() {
       {/* --- EXTENDS --- */}
       <Grid width='full' gap={6} mt={12} className='grid-cols-instructions'>
         <div>
-          <Text variant='subtitle'>2. Extend theme values</Text>
+          <Text variant='subtitle'>3. Extend theme values</Text>
           <Text mt={2} ml={2}>
             Default theme values can be extended using the <code>extend</code> field, this
             allows you to add custom theme values to the Componentry default set.
           </Text>
         </div>
         <CodeBlock code={extend} language='tsx' />
+      </Grid>
+      {/* --- AUGMENT --- */}
+      <Grid width='full' gap={6} mt={12} className='grid-cols-instructions'>
+        <div>
+          <Text variant='subtitle'>4. Augment types</Text>
+          <Text mt={2} ml={2}>
+            The componentry <code>Theme</code> type can be customized using{' '}
+            <Link href='https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation'>
+              module augmentation
+            </Link>
+            . The simplest way to do this is to use the <code>typeof</code> operator with
+            your custom theme values.
+          </Text>
+        </div>
+        <CodeBlock code={augment} language='tsx' />
       </Grid>
       {/* --- DEFAULTS --- */}
       <ContentSection>
